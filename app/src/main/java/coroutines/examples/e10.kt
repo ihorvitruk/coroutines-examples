62a3f523272d1e0fdb82b872dda936cd6d894ad2
Ihor Vitruk <ihorvitruk@gmail.com>
2018-12-05 00:29:03 +0300

    add e10

--- app/src/main/java/coroutines/examples/e10.kt
+++ app/src/main/java/coroutines/examples/e10.kt
@@ -5,7 +5,10 @@ import kotlinx.coroutines.*
 fun main() = runBlocking {
     val job = launch(Dispatchers.Default) {
         Thread.sleep(2000L) //emulate some i/o or computation
-        println("Message after emulated work") //let it be update UI "null" view after activity close.
+        yield()
+        //if (isActive) {
+            println("Message after emulated work") //let it be update UI "null" view after activity close.
+        //}
     }
     delay(1000L)
     println("Cancel job")
